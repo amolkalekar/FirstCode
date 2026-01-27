bbc012f9d374c818b954551a9a51652484082afc
Amol Kalekar <amolkalekar78@gmail.com>
2026-01-27 18:51:37 +0530

    added guess the number function
    added guess the number function

--- Program.cs
+++ Program.cs
@@ -7,6 +7,34 @@ namespace BasicCodes
                Console.WriteLine("Hello");
         }
 
+  private static void GuessTheNumber()
+  {
+      Random rnd = new Random();
+      int on = rnd.Next(1, 101);
+
+      int un = 0;
+      while (true)
+      {
+          Console.WriteLine("Guess the number.");
+          un = Convert.ToInt32(Console.ReadLine());
+
+          if (un == on)
+          {
+              Console.WriteLine("You got the right number.");
+              break;
+          }
+          else if (un < on)
+          {
+              Console.WriteLine("you guessed it Low");
+          }
+          else
+          {
+              Console.WriteLine("you guessed it high");
+          }
+      }
+  }
+
+        
          private static void FindMaxIn3Numbers()
  {
      Console.WriteLine("Enter Number 1 ");
