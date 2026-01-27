805cad12dd9818acd70f0a1c04fe87469c3c37eb
Amol Kalekar <amolkalekar78@gmail.com>
2026-01-27 18:18:37 +0530

    Added FindMaxIn3Numbers() to the file
    Added FindMaxIn3Numbers() to the file

--- Program.cs
+++ Program.cs
@@ -6,5 +6,28 @@ namespace BasicCodes
         {
                Console.WriteLine("Hello");
         }
+
+         private static void FindMaxIn3Numbers()
+ {
+     Console.WriteLine("Enter Number 1 ");
+     int a = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Enter Number 2 ");
+     int b = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Enter Number 3 ");
+     int c = Convert.ToInt32(Console.ReadLine());
+
+     int max = a;
+
+     if (max < b)
+     {
+         max = b;
+     }
+     if (max < c)
+     {
+         max = c;
+     }
+
+     HighlightLine("MAX Value is " + max);
+ }
     }
 }
